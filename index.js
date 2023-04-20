5a6db830e4d93e85acee9f26a585e282d518c17d
AP696 <addyphilips66@gmail.com>
2023-04-20 15:40:50 +0100

    register, login

--- index.js
+++ index.js
@@ -16,9 +16,35 @@ app.get('/', async (req, res, next) => {
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post('/register'), async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPw = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPw });
+    res.send(user, "Success");
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (user) {
+      const hashedPw = user.password;
+      const passwordsMatch = await bcrypt.compare((password, hashedPw));
+      if (passwordsMatch) {
+        res.send(user, "Success")
+      }
+    }
+  } catch (error) {
+    
+  }
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
